5c24284fbca8909f875343b2a0b405fe72b23872
triz1e2024 <aparecido.isabella@escola.pr.gov.br>
2026-05-10 20:56:41 -0300

    Update main.js

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2026-06-01T00:00:00");
-const tempoObjetivo2 = new Date("2026-12-25T00:00:00");
+const tempoObjetivo2 = new Date("2026-11-08T00:00:00");
 const tempoObjetivo3 = new Date("2026-12-01T00:00:00");
 const tempoObjetivo4 = new Date("2026-05-28T00:00:00");
 
